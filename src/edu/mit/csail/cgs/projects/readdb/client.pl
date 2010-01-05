1200130b560fd1dc7f9d776e576338976a3b7848
Alex Rolfe <arolfe@mit.edu>
2010-01-05 18:30:14 -0500

    perl client library for ReadDB and the beginnings of a command line client.

---
+++ src/edu/mit/csail/cgs/projects/readdb/client.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use ReadDBClient;
+
+my $client = new ReadDBClient();
+my $cmd = shift(@ARGV);
+
+if ($cmd eq 'getChroms') {
+  my $align = shift(@ARGV);
+  my @c = $client->getChroms($align);
+  print "chroms for $align are @c\n";
+} elsif ($cmd eq 'getCount') {
+  my $align = shift(@ARGV);
+  my $c = $client->getCount($align);
+  print "count for $align is $c\n";
+}
