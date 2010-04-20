6cc8a25dc973831f2416099aa63966cf0ca28e66
Shaun Mahony <mahony@csail.mit.edu>
2010-04-20 17:50:21 -0400

    Fixed & extended Tim's K-means implementation and fixed a horrible bug in EuclideanDistance

--- src/edu/mit/csail/cgs/clustering/vectorcluster/EuclideanDistance.java
+++ src/edu/mit/csail/cgs/clustering/vectorcluster/EuclideanDistance.java
@@ -7,13 +7,10 @@ public class EuclideanDistance<X extends VectorClusterElement> implements Pairwi
     public double evaluate(X e1, X e2) {
         if(e1.dimension() != e2.dimension()) { throw new IllegalArgumentException(); }
         double value = 0.0;
-			
+        
         for(int i = 0; i < e1.dimension(); i++) { 
             double s = 0.0;
-            if(!e1.isMissingValue(i) || !e2.isMissingValue(i)) { 
-                if(!e1.isMissingValue(i)) { s = e1.getValue(i); }
-                if(!e2.isMissingValue(i)) { s = e2.getValue(i); }
-            } else { 
+            if(!e1.isMissingValue(i) && !e2.isMissingValue(i)) { 
                 s = (e1.getValue(i) - e2.getValue(i));
             }
             value += (s * s);
