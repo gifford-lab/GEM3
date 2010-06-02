1535b3e70e74ce9005b8746a177a559eb39a0fe9
ccr <ccr@csail.mit.edu>
2010-06-01 21:59:38 -0400

    hashcode for PairedHit

--- src/edu/mit/csail/cgs/projects/readdb/PairedHit.java
+++ src/edu/mit/csail/cgs/projects/readdb/PairedHit.java
@@ -36,7 +36,21 @@ public class PairedHit {
             return false;
         }
     }   
-    public void flipSides() {
+    
+    @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + leftChrom;
+		result = prime * result + leftPos;
+		result = prime * result + (leftStrand ? 1231 : 1237);
+		result = prime * result + rightChrom;
+		result = prime * result + rightPos;
+		result = prime * result + (rightStrand ? 1231 : 1237);
+		return result;
+	}
+
+	public void flipSides() {
         int x = leftChrom;
         leftChrom = rightChrom;
         rightChrom = x;
