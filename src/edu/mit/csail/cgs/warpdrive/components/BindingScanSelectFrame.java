fa35a4fbfeee03c381581632d57fd3a450b54253
Alex Rolfe <arolfe@mit.edu>
2010-07-23 15:38:57 -0400

    working on navigation and analysis of chip-seq binding scans

--- src/edu/mit/csail/cgs/warpdrive/components/BindingScanSelectFrame.java
+++ src/edu/mit/csail/cgs/warpdrive/components/BindingScanSelectFrame.java
@@ -12,6 +12,8 @@ import edu.mit.csail.cgs.datasets.binding.BindingEvent;
 import edu.mit.csail.cgs.datasets.binding.BindingScan;
 import edu.mit.csail.cgs.datasets.binding.BindingScanLoader;
 import edu.mit.csail.cgs.datasets.species.Genome;
+import edu.mit.csail.cgs.datasets.general.Region;
+import edu.mit.csail.cgs.datasets.chipseq.*;
 import edu.mit.csail.cgs.ewok.verbs.ChromRegionIterator;
 import edu.mit.csail.cgs.ewok.verbs.binding.BindingExpander;
 import edu.mit.csail.cgs.utils.database.UnknownRoleException;
@@ -22,12 +24,13 @@ public class BindingScanSelectFrame extends JFrame implements ActionListener {
 
     private Genome genome;
     private BindingScanSelectPanel bssp;
+    private ChipSeqAnalysisSelectPanel csasp;
     private JButton ok, cancel;    
     private RegionList list;
 
     public static void main(String args[]) throws Exception {
         Genome genome = new Genome(args[0],args[1]);
-        BindingEventAnnotationPanel beap = new BindingEventAnnotationPanel(null, new ArrayList<BindingEvent>());
+        BindingEventAnnotationPanel beap = new BindingEventAnnotationPanel(null, new ArrayList<BindingEvent>());        
         JFrame f = new BindingEventAnnotationPanel.Frame(beap);
         f.pack();
         new BindingScanSelectFrame(genome,beap);
@@ -38,6 +41,7 @@ public class BindingScanSelectFrame extends JFrame implements ActionListener {
 
         try {
 			bssp = new BindingScanSelectPanel();
+            csasp = new ChipSeqAnalysisSelectPanel();
 		} catch (UnknownRoleException e) {
 			e.printStackTrace();
 			throw new IllegalArgumentException(e.getMessage());
@@ -59,6 +63,8 @@ public class BindingScanSelectFrame extends JFrame implements ActionListener {
         this.genome = g;
         
         JPanel buttonPanel = new JPanel();
+        JTabbedPane tabbed = new JTabbedPane();
+
         buttonPanel.setLayout(new GridBagLayout());
         Dimension buttonSize = new Dimension(30,20);
         ok = new JButton("OK");
@@ -69,10 +75,14 @@ public class BindingScanSelectFrame extends JFrame implements ActionListener {
         cancel.addActionListener(this);
         buttonPanel.add(ok);
         buttonPanel.add(cancel);
+
+        tabbed.addTab("ChIP-Chip", bssp);
+        tabbed.addTab("ChIP-Seq",csasp);
+
         Container content = getContentPane();
         content.setLayout(new BorderLayout());
         content.add(buttonPanel,BorderLayout.SOUTH);
-        content.add(bssp,BorderLayout.CENTER);
+        content.add(tabbed,BorderLayout.CENTER);
         setSize(500,400);
         setLocation(50,50);
         setVisible(true);
@@ -81,7 +91,8 @@ public class BindingScanSelectFrame extends JFrame implements ActionListener {
     public void actionPerformed (ActionEvent e) {
         if (e.getSource() == ok) {
             Collection<BindingScan> scans = bssp.getObjects();
-            Populator p = new Populator(scans,genome);
+            Collection<ChipSeqAnalysis> analyses = csasp.getObjects();
+            Populator p = new Populator(scans,analyses,genome);
             Thread t = new Thread(p);
             t.start();
             this.dispose();
@@ -93,29 +104,40 @@ public class BindingScanSelectFrame extends JFrame implements ActionListener {
     private class Populator implements Runnable {
         
         private Collection<BindingScan> scans;
+        private Collection<ChipSeqAnalysis> analyses;
         private Genome genome;
         
-        public Populator(Collection<BindingScan> scans, Genome g) {
+        public Populator(Collection<BindingScan> scans, Collection<ChipSeqAnalysis> analyses, Genome g) {
             this.scans = scans;
+            this.analyses = analyses;
             genome = g;
         }
         
         public void run() {
             try {
                 BindingScanLoader loader = new BindingScanLoader();
-                
+
+                Collection<BindingExpander> expanders = new ArrayList<BindingExpander>();
                 for (BindingScan scan : scans) {
                     BindingExpander expander = new BindingExpander(loader,scan);
+                    expanders.add(expander);
+                }
 
-                    ChromRegionIterator iter = new ChromRegionIterator(genome);
-                    while (iter.hasNext()) {
-                        Iterator<BindingEvent> events = expander.execute(iter.next());
+                ChromRegionIterator iter = new ChromRegionIterator(genome);                
+                while (iter.hasNext()) {
+                    Region chrom = iter.next();
+                    for (BindingExpander expander : expanders) {
+                        Iterator<BindingEvent> events = expander.execute(chrom);
                         while (events.hasNext()) {
                             list.addRegion(events.next());
                         }
                     }
-                }
-                
+                    for (ChipSeqAnalysis analysis : analyses) {
+                        for (ChipSeqAnalysisResult r : analysis.getResults(genome, chrom)) {
+                                list.addRegion(r);
+                        }
+                    }
+                }                               
                 loader.close();
 
             } catch (Exception ex) {
