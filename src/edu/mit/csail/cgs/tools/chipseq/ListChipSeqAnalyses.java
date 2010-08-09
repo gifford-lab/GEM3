66fa3fe591620c24e0b902f02f10f54306322004
Alex Rolfe <arolfe@mit.edu>
2010-08-09 15:52:35 -0400

    added option for html output

--- src/edu/mit/csail/cgs/tools/chipseq/ListChipSeqAnalyses.java
+++ src/edu/mit/csail/cgs/tools/chipseq/ListChipSeqAnalyses.java
@@ -1,28 +1,59 @@
 package edu.mit.csail.cgs.tools.chipseq;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.ArrayList;
 import java.sql.SQLException;
 import edu.mit.csail.cgs.datasets.species.*;
 import edu.mit.csail.cgs.datasets.chipseq.*;
 import edu.mit.csail.cgs.tools.utils.Args;
 
+/**
+ * output fields are
+ * 0) name
+ * 1) version
+ * 2) program name
+ * 3) event count
+ * 4) fg expts
+ * 5) bg expts
+ *
+ */
+
 public class ListChipSeqAnalyses {
 
     public static void main(String[] args) throws Exception {
         Genome genome = Args.parseGenome(args).cdr();
-        Collection<ChipSeqAnalysis> all = ChipSeqAnalysis.getAll();
+        boolean html = Args.parseFlags(args).contains("html");
+        ArrayList<ChipSeqAnalysis> all = new ArrayList<ChipSeqAnalysis>();
+        all.addAll(ChipSeqAnalysis.getAll());
+        Collections.sort(all);
+        if (html) {
+            System.out.println("<tr><th>Name</th><th>Version</th><th>Analysis Program</th><th>Number of Binding Events</th><th>IP Experiments</th><th>Control Experimentss</th></tr>");
+        } 
+
+
         for (ChipSeqAnalysis a : all) {
             int count = a.countResults(genome);
             if (count == 0) {
                 continue;
             }
-            System.err.println(String.format("%s\t%s\t%s\t%d\t%s\t%s",
-                                             a.getName(),
-                                             a.getVersion(),
-                                             a.getProgramName(),
-                                             count,
-                                             a.getForeground().toString(),
-                                             a.getBackground().toString()));
+            if (html) {
+                System.out.println(String.format("<tr><th>%s</th><th>%s</th><th>%s</th><th>%d</th><th>%s</th><th>%s</th></tr>",
+                                                 a.getName(),
+                                                 a.getVersion(),
+                                                 a.getProgramName(),
+                                                 count,
+                                                 a.getForeground().toString(),
+                                                 a.getBackground().toString()));
+            } else {
+                System.out.println(String.format("%s\t%s\t%s\t%d\t%s\t%s",
+                                                 a.getName(),
+                                                 a.getVersion(),
+                                                 a.getProgramName(),
+                                                 count,
+                                                 a.getForeground().toString(),
+                                                 a.getBackground().toString()));
+            }
         }
     }
 
