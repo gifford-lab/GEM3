e5218a71a78ac950cb8d0333d4b67f12bcc34bc0
Shaun Mahony <mahony@csail.mit.edu>
2010-10-23 18:45:20 -0400

    Changed output format to follow stats script we use for bowtie. Moved to ReadDB

--- src/edu/mit/csail/cgs/projects/readdb/SAMStats.java
+++ src/edu/mit/csail/cgs/projects/readdb/SAMStats.java
@@ -1,4 +1,4 @@
-package edu.mit.csail.cgs.deepseq.utilities;
+package edu.mit.csail.cgs.projects.readdb;
 
 import java.io.IOException;
 
@@ -17,11 +17,13 @@ import edu.mit.csail.cgs.utils.RealValuedHistogram;
 
 public class SAMStats {
 
-	private int totalHits=0, LHits=0, RHits=0;
+	private int totalHits=0, LHits=0, RHits=0, totalHitBP=0, LHitBP=0, RHitBP=0;
 	private int singleEnd=0, properPair=0, unMapped=0, properPairL=0, properPairR=0, pairMapped=0, notPrimary=0;
-	private int uniquelyMapped=0;
-	private double weight = 0;
+	private int singleEndBP=0, properPairBP=0, unMappedBP=0, properPairLBP=0, properPairRBP=0, pairMappedBP=0, notPrimaryBP=0;
+	private int uniquelyMapped=0, uniquelyMappedBP=0;
+	private double weight=0, weightBP=0;;
 	private int pairedEndSameChr=0, pairedEndDiffChr=0;
+	private int pairedEndSameChrBP=0, pairedEndDiffChrBP=0;
 	private RealValuedHistogram histo;
 	
 	public static void main(String args[]) throws IOException, ParseException {
@@ -56,32 +58,46 @@ public class SAMStats {
 			unMapped++;
 		else{
 			totalHits++;
+			int len = r.getReadLength();
+			totalHitBP+=len;
 			int count = r.getIntegerAttribute("NH");
-			if(count==1)
+			if(count==1){
 				uniquelyMapped++;
+				uniquelyMappedBP+=len;
+			}
 			
 			weight += 1/(float)count;
+			weightBP += (1/(float)count)*len;
 			
 			if(r.getReadPairedFlag()){
 				if(r.getMateUnmappedFlag()){
 					singleEnd++;
+					singleEndBP+=len;
 				}else{
 					pairMapped++;
-					if(r.getMateReferenceName().equals(r.getReferenceName()))
+					pairMappedBP+=len;
+					if(r.getMateReferenceName().equals(r.getReferenceName())){
 						pairedEndSameChr++;
-					else
+						pairedEndSameChrBP+=len;
+					}else{
 						pairedEndDiffChr++;
+						pairedEndDiffChrBP+=len;
+					}
 				}
 			}else{
 				singleEnd++;
+				singleEndBP+=len;
 			}
 			
 			if(!r.getNotPrimaryAlignmentFlag()){
 				if(!r.getReadPairedFlag() || r.getFirstOfPairFlag()){
 					LHits++;
+					LHitBP+=len;
 					if(r.getReadPairedFlag() && r.getProperPairFlag()){
 						properPairL++;
+						properPairLBP+=len;
 						properPair++;
+						properPairBP+=len;
 						if(!r.getReadNegativeStrandFlag() && r.getMateNegativeStrandFlag()){
 							double dist = (r.getMateAlignmentStart()+r.getReadLength())-r.getAlignmentStart();
 							histo.addValue(dist);
@@ -89,13 +105,17 @@ public class SAMStats {
 					}
 				}else if(r.getSecondOfPairFlag()){
 					RHits++;
+					RHitBP+=len;
 					if(r.getProperPairFlag()){
 						properPairR++;
+						properPairRBP+=len;
 						properPair++;
+						properPairBP+=len;
 					}
 				}
 			}else{
 				notPrimary++;
+				notPrimaryBP+=len;
 			}
 		}
 	}
@@ -106,21 +126,22 @@ public class SAMStats {
 	}
 
 	public void printStats(){
-		System.out.println("\nTotal Mappings:\t"+totalHits);
-		System.out.println("Total Weight:\t"+(int)weight);
-		System.out.println("Uniquely Mapped:\t"+uniquelyMapped);
+		System.out.println("\nTotalHits:\t"+totalHits+"\t"+totalHitBP+" bp");
+		System.out.println("LeftHits\t"+LHits+"\t"+LHitBP+" bp");
+		System.out.println("RightHits\t"+RHits+"\t"+RHitBP+" bp");
+		System.out.println("MappedSeq:\t"+(int)weight+"\t"+weightBP+" bp");
+		System.out.println("UniquelyMapped:\t"+uniquelyMapped+"\t"+uniquelyMappedBP+" bp");
 		int nonU = (int)weight - uniquelyMapped;
-		System.out.println("Non-uniquely Mapped:\t"+nonU);
-		System.out.println("Left Mappings\t"+LHits);
-		System.out.println("Right Mappings\t"+RHits);
-		System.out.println("SingleEnd Mappings:\t"+singleEnd);
-		System.out.println("PairedEnd Mappings\t"+pairMapped);
-		System.out.println("Proper pairs:\t"+properPair);
-		System.out.println("Proper pairs (L):\t"+properPairL);
-		System.out.println("Proper pairs (R):\t"+properPairR);
-		System.out.println("PairedEnd Mappings (same chr):\t"+pairedEndSameChr);
-		System.out.println("PairedEnd Mappings (diff chr):\t"+pairedEndDiffChr);
-		System.out.println("UnMapped:\t"+unMapped);
-		System.out.println("NotPrimary:\t"+notPrimary);
+		int nonUBP = (int)weightBP - uniquelyMappedBP;
+		System.out.println("NonUniquelyMapped:\t"+nonU+"\t"+nonUBP+" bp");
+		System.out.println("SingleEndMapped:\t"+singleEnd+"\t"+singleEndBP+" bp");
+		System.out.println("PairedEndMapped\t"+pairMapped+"\t"+pairMappedBP+" bp");
+		System.out.println("ProperPairs:\t"+properPair+"\t"+properPairBP+" bp");
+		System.out.println("ProperPairsL:\t"+properPairL+"\t"+properPairLBP+" bp");
+		System.out.println("ProperPairsR:\t"+properPairR+"\t"+properPairRBP+" bp");
+		System.out.println("PairedEndMapped_SameChr:\t"+pairedEndSameChr+"\t"+pairedEndSameChrBP+" bp");
+		System.out.println("PairedEndMapped_DiffChr:\t"+pairedEndDiffChr+"\t"+pairedEndDiffChrBP+" bp");
+		System.out.println("UnMapped:\t"+unMapped+"\t"+unMappedBP+" bp");
+		System.out.println("NotPrimary:\t"+notPrimary+"\t"+notPrimaryBP+" bp");
 	}
 }
