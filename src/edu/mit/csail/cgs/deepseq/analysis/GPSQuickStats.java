435fd028101ba79a2f2067b58894f8f4e92aa0c8
Alex Rolfe <arolfe@mit.edu>
2010-12-01 20:41:55 -0500

    quick compilation fix

--- src/edu/mit/csail/cgs/deepseq/analysis/GPSQuickStats.java
+++ src/edu/mit/csail/cgs/deepseq/analysis/GPSQuickStats.java
@@ -94,7 +94,8 @@ public class GPSQuickStats{
 //	});
 	
 	GPSOutputAnalysis gpsAnalysis = new GPSOutputAnalysis(genome, motif, motifThreshold, gpsPeaks, 
-			"Analysis_"+GPSfileName.substring(0, GPSfileName.length()-4), motif_window);
+                                                          "Analysis_"+GPSfileName.substring(0, GPSfileName.length()-4), 
+                                                          motif_window,0);
 	String stats = gpsAnalysis.printMotifHitList();
 	String msg = "\n"+CommonUtils.timeElapsed(tic)+ "\n";
 	System.out.print(msg);
