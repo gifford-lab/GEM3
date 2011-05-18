36cb98e5614add4dfb7d302ed2c3a1ecb5c1660d
Yuchun Guo <yguo@mit.edu>
2011-05-18 17:41:18 -0400

    update

--- src/edu/mit/csail/cgs/deepseq/analysis/EnhancerChromatinAnalysis.java
+++ src/edu/mit/csail/cgs/deepseq/analysis/EnhancerChromatinAnalysis.java
@@ -6,62 +6,38 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.Vector;
 
 import edu.mit.csail.cgs.datasets.chipseq.ChipSeqLocator;
 import edu.mit.csail.cgs.datasets.general.Point;
 import edu.mit.csail.cgs.datasets.general.Region;
-import edu.mit.csail.cgs.datasets.motifs.WeightMatrix;
 import edu.mit.csail.cgs.datasets.species.Genome;
 import edu.mit.csail.cgs.datasets.species.Organism;
 import edu.mit.csail.cgs.deepseq.BindingModel;
 import edu.mit.csail.cgs.deepseq.DeepSeqExpt;
 import edu.mit.csail.cgs.deepseq.StrandedBase;
-import edu.mit.csail.cgs.deepseq.features.ComponentFeature;
-import edu.mit.csail.cgs.deepseq.features.Feature;
 import edu.mit.csail.cgs.deepseq.utilities.CommonUtils;
 import edu.mit.csail.cgs.deepseq.utilities.ReadCache;
-import edu.mit.csail.cgs.deepseq.utilities.CommonUtils.SISSRS_Event;
 import edu.mit.csail.cgs.ewok.verbs.chipseq.GPSParser;
 import edu.mit.csail.cgs.ewok.verbs.chipseq.GPSPeak;
-import edu.mit.csail.cgs.ewok.verbs.chipseq.MACSParser;
-import edu.mit.csail.cgs.ewok.verbs.chipseq.MACSPeakRegion;
-import edu.mit.csail.cgs.ewok.verbs.motifs.WeightMatrixScoreProfile;
-import edu.mit.csail.cgs.ewok.verbs.motifs.WeightMatrixScorer;
 import edu.mit.csail.cgs.tools.utils.Args;
 import edu.mit.csail.cgs.utils.ArgParser;
 import edu.mit.csail.cgs.utils.NotFoundException;
 import edu.mit.csail.cgs.utils.Pair;
-import edu.mit.csail.cgs.utils.SetTools;
 import edu.mit.csail.cgs.utils.stats.StatUtil;
 
 public class EnhancerChromatinAnalysis {
-	static String h3k27ac_esc = "Wysocka hESC H3K27ac H9;prealigned_unique";
-	static String h3k27me3_esc = "Wysocka hESC H3K27me3 H9;prealigned_unique";
-	static String h3k4me3_esc = "Wysocka hESC H3K4me3 H9;prealigned_unique";
-	static String h3k4me1_esc = "Wysocka hESC H3K4me1 H9;prealigned_unique";
-	static String input_esc = "Wysocka hESC input H9;prealigned_unique";
-	static String h3k27ac_nec = "Wysocka hNEC H3K27ac H9;prealigned_unique";
-	static String h3k27me3_nec = "Wysocka hNEC H3K27me3 H9;prealigned_unique";
-	static String h3k4me3_nec = "Wysocka hNEC H3K4me3 H9;prealigned_unique";
-	static String h3k4me1_nec = "Wysocka hNEC H3K4me1 H9;prealigned_unique";	
-	static String input_nec = "Wysocka hNEC input H9;prealigned_unique";
-	
-	private final int NOHIT_OFFSET = 999;
+	private Genome genome;
+	private String[] args;
 	
 	private boolean dev = false;
 	private boolean isPreSorted = false;	
 	private int windowSize = 1000;	
+	private int modelRange = 1500;	
 	private int rank = 1000;
 	private int smooth_step = 30;
 	private boolean sortByStrength = false;
-	
-	private Genome genome;
-	private Organism org;
-	private String[] args;
 	private String outName="out";
 	
 	// each element in the list is for one ChIP-Seq method
@@ -69,6 +45,8 @@ public class EnhancerChromatinAnalysis {
 	private HashMap<String, Integer> markIDs = new HashMap<String, Integer>();
 	private ArrayList<ReadCache> caches = null;
 	private ArrayList<Point> events = new ArrayList<Point>();
+	ArrayList<Point> classI = new ArrayList<Point>();
+	ArrayList<Point> classII = new ArrayList<Point>();
 	private double[][] profiles_I;
 	private double[][] profiles_II;
 
@@ -81,7 +59,6 @@ public class EnhancerChromatinAnalysis {
 		int type = Args.parseInteger(args, "analysisType", 0);
 		switch(type){
 		case 0:analysis.findEnhancers();break;
-		case 1:analysis.computeLikelihoodRatio();break;
 		default: System.err.println("Unrecognize analysis type: "+type);
 		}
 	}
@@ -93,15 +70,13 @@ public class EnhancerChromatinAnalysis {
 	    try {
 	      Pair<Organism, Genome> pair = Args.parseGenome(args);
 	      if(pair==null){
-	        //Make fake genome... chr lengths provided???
 	        if(ap.hasKey("geninfo")){
 	          genome = new Genome("Genome", new File(ap.getKeyValue("geninfo")));
-	            }else{
-	              System.err.println("No genome provided; provide a Gifford lab DB genome name or a file containing chromosome name/length pairs.");;System.exit(1);
-	            }
+	        }else{
+              System.err.println("No genome provided; provide a Gifford lab DB genome name or a file containing chromosome name/length pairs.");;System.exit(1);
+            }
 	      }else{
 	        genome = pair.cdr();
-	        org = pair.car();
 	      }
 	    } catch (NotFoundException e) {
 	      e.printStackTrace();
@@ -109,6 +84,7 @@ public class EnhancerChromatinAnalysis {
 	    	    
 		// some parameters
 		windowSize = Args.parseInteger(args, "windowSize", windowSize);
+		modelRange = Args.parseInteger(args, "modelRange", modelRange);
 		rank = Args.parseInteger(args, "rank", rank);
 		smooth_step = Args.parseInteger(args, "smooth", smooth_step);
 		outName = Args.parseString(args,"out",outName);
@@ -141,9 +117,6 @@ public class EnhancerChromatinAnalysis {
 	// This method print 2 text files: class I and II enhancer coordinates
 	private void findEnhancers() throws IOException {
 		long tic = System.currentTimeMillis();
-		
-		ArrayList<Point> classI = new ArrayList<Point>();
-		ArrayList<Point> classII = new ArrayList<Point>();
 		for (Point p: events){
 			Region r = p.expand(windowSize);
 			if (isEnriched(r, "H3K4me1", "input", 10) && isEnriched(r, "H3K27ac", "input", 10) && (!isEnriched(r, "H3K4me3", "input", 30)))
@@ -158,7 +131,7 @@ public class EnhancerChromatinAnalysis {
 			sb.append(p.toString()).append("\n");
 		CommonUtils.writeFile(outName+"_enhancer_I_coords.txt", sb.toString());
 		
-		generateProfiles("I", classI, profiles_I, windowSize, windowSize);
+		generateProfiles("I", classI, profiles_I, modelRange, modelRange);
 		
 		System.out.println(outName+"_enhancer_II:\t"+classII.size());
 		sb = new StringBuilder();
@@ -166,11 +139,13 @@ public class EnhancerChromatinAnalysis {
 			sb.append(p.toString()).append("\n");
 		CommonUtils.writeFile(outName+"_enhancer_II_coords.txt", sb.toString());
 		
-		generateProfiles("II", classII, profiles_II, windowSize, windowSize);
+		generateProfiles("II", classII, profiles_II, modelRange, modelRange);
 
+		computeLikelihoodRatio("I", classI);
+		computeLikelihoodRatio("II", classII);
+		
 		System.out.println("Done! " + CommonUtils.timeElapsed(tic));
 	}
-
 	
 	private boolean isEnriched(Region region, String ip, String ctrl, double ip_min){
 		float ip_count = caches.get(markIDs.get(ip)).countHits(region);
@@ -367,61 +342,31 @@ public class EnhancerChromatinAnalysis {
 	}
 	
 	// Compute the likelihood ratio of a p300 event region fitting to class I and II enhancer profiles
-	private void computeLikelihoodRatio() throws IOException {
+	private void computeLikelihoodRatio(String classType, ArrayList<Point> coords) throws IOException {
 		long tic = System.currentTimeMillis();
-		readPeakLists();
-		
-		BindingModel h3k4me1_I = getProfile("Read_Distribution_h3k4me1_esc_I_1000.txt");
-		BindingModel h3k4me3_I = getProfile("Read_Distribution_h3k4me3_esc_I_1000.txt");
-		BindingModel h3k27me3_I = getProfile("Read_Distribution_h3k27me3_esc_I_1000.txt");
-		BindingModel h3k27ac_I = getProfile("Read_Distribution_h3k27ac_esc_I_1000.txt");
-
-		BindingModel h3k4me1_II = getProfile("Read_Distribution_h3k4me1_esc_II_1000.txt");
-		BindingModel h3k4me3_II = getProfile("Read_Distribution_h3k4me3_esc_II_1000.txt");
-		BindingModel h3k27me3_II = getProfile("Read_Distribution_h3k27me3_esc_II_1000.txt");
-		BindingModel h3k27ac_II = getProfile("Read_Distribution_h3k27ac_esc_II_1000.txt");
 
 		StringBuilder sb = new StringBuilder();
-		sb.append("Event\t\tk4me1I\tk4me1II\tk4me3I\tk4me3II\tk27me3I\tk27m3II\tk27acI\tk27acII\tOverall\tK27m3ac\n");
-//		for (Point p: events){
-//			sb.append(p.toString()+"\t");
-//			Pair<Double, Double> pair1 = getLikelihood(p, h3k4me1, h3k4me1_I, h3k4me1_II);
-//			sb.append(String.format("%.1f\t%.1f\t", pair1.car(), pair1.cdr()));
-//			Pair<Double, Double> pair2 = getLikelihood(p, h3k4me3, h3k4me3_I, h3k4me3_II);
-//			sb.append(String.format("%.1f\t%.1f\t", pair2.car(), pair2.cdr()));
-//			Pair<Double, Double> pair3 = getLikelihood(p, h3k27me3, h3k27me3_I, h3k27me3_II);
-//			sb.append(String.format("%.1f\t%.1f\t", pair3.car(), pair3.cdr()));
-//			Pair<Double, Double> pair4 = getLikelihood(p, h3k27ac, h3k27ac_I, h3k27ac_II);
-//			sb.append(String.format("%.1f\t%.1f\t", pair4.car(), pair4.cdr()));
-//			double total = pair1.car()-pair1.cdr()+pair2.car()-pair2.cdr()+pair3.car()-pair3.cdr()+pair4.car()-pair4.cdr();
-//			double k27 = pair3.car()-pair3.cdr()+pair4.car()-pair4.cdr();
-//			sb.append(String.format("%.1f\t%.1f\n", total, k27));
-//		}
-		CommonUtils.writeFile("enhancer_all_likelihood_ratio.txt", sb.toString());
+		sb.append("Event\t\tll_I\tll_II\tllr\n");
+		for (Point p: coords){
+			sb.append(p.toString()+"\t");
+			int pos = p.getLocation();
+			double ll_I = 0;
+			double ll_II = 0;
+			for (int c=0;c<profiles_I.length;c++){
+				List<StrandedBase> bases = caches.get(c).getStrandedBases(p.expand(modelRange), '+');
+				bases.addAll(caches.get(c).getStrandedBases(p.expand(modelRange), '-'));
+				for(StrandedBase b : bases){
+					int dist = b.getStrand()=='+' ? b.getCoordinate()-pos:pos-b.getCoordinate();
+					int idx = dist + modelRange;
+					ll_I += Math.log(profiles_I[c][idx])*b.getCount();
+					ll_II += Math.log(profiles_II[c][idx])*b.getCount();
+				}
+			}
+			double llr = ll_I - ll_II;
+			sb.append(String.format("%.1f\t%.1f\t%.1f\n", ll_I, ll_II, llr));
+		}
+		CommonUtils.writeFile(outName+"_"+classType+"_likelihood_ratio.txt", sb.toString());
 		
 		System.out.println("Done! " + CommonUtils.timeElapsed(tic));
 	}
-	private BindingModel getProfile(String modelFile){
-		//Load Binding Model
-		File pFile = new File(modelFile);
-		if(!pFile.isFile()){
-			System.err.println("\nCannot find read distribution file!");
-			System.exit(1);
-		}
-        return new BindingModel(pFile);
-	}
-	private Pair<Double, Double> getLikelihood(Point p, DeepSeqExpt ip, BindingModel classI, BindingModel classII){
-		double ll_I = 0;
-		double ll_II = 0;
-		int pos = p.getLocation();
-		Pair<ArrayList<Integer>,ArrayList<Float>> pair = ip.loadStrandedBaseCounts(p.expand(1500), '+');
-		for (int i=0;i<pair.car().size();i++){
-			ll_I += Math.log10(classI.probability(pair.car().get(i)-pos))*pair.cdr().get(i);
-			ll_II += Math.log10(classII.probability(pair.car().get(i)-pos))*pair.cdr().get(i);
-		}
-		return new Pair<Double, Double>(ll_I, ll_II);
-	}
-	
-
-
 }
