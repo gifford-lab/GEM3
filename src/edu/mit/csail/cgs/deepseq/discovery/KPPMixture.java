110875009d5c8c4464d8b81e5e79188164000227
Yuchun Guo <yguo@mit.edu>
2013-02-23 18:49:20 -0500

    only do EM speed_up at coarse resolution

--- src/edu/mit/csail/cgs/deepseq/discovery/KPPMixture.java
+++ src/edu/mit/csail/cgs/deepseq/discovery/KPPMixture.java
@@ -4586,30 +4586,35 @@ public class KPPMixture extends MultiConditionFeatureFinder {
                 }
                 
                 // EM speed up, eliminate components with probability less than initial avg prob.
+                // Only do this for coase spacing, which has more components
                 double minProb = 1.0/(pi.length*2);		// length*2 to make sure it is smaller than 1/m
-                if (t<=config.ML_ITER){
+                if (t<=config.ML_ITER && (componentSpacing!=1)){
+                	boolean eliminated = false;
                 	for (int j:nzComps){
                 		if (pi[j]<minProb){
                 			// eliminate component, clear responsibility
                 			pi[j]=0;
+                			eliminated = true;
                 			for(int c=0; c<mixture.numConditions; c++){
                                 for(int i=0; i<c2b[c][j].length;i++)
                                     r[c][j][i] = 0;
                             }
                 		}
                 	}
-                	 // normalize pi here again
-                    totalPi=0;
-                    for(int j:nzComps){
-                        if (pi[j]!=0){
-                            totalPi+=pi[j];
-                        }
-                    }            	
-                    if (totalPi!=0){
-                        for(int j:nzComps){
-                            pi[j]=pi[j]/totalPi;
-                        }
-                    }
+                	if (eliminated){
+	                	 // normalize pi here again
+	                    totalPi=0;
+	                    for(int j:nzComps){
+	                        if (pi[j]!=0){
+	                            totalPi+=pi[j];
+	                        }
+	                    }            	
+	                    if (totalPi!=0){
+	                        for(int j:nzComps){
+	                            pi[j]=pi[j]/totalPi;
+	                        }
+	                    }
+                	}
                 }
                 
                 // update component count
