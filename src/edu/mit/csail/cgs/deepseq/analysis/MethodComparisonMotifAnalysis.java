f637f66149fef8b2e51423db603e6332daae6366
Yuchun Guo <yguo@mit.edu>
2013-05-24 18:12:56 -0400

    update

--- src/edu/mit/csail/cgs/deepseq/analysis/MethodComparisonMotifAnalysis.java
+++ src/edu/mit/csail/cgs/deepseq/analysis/MethodComparisonMotifAnalysis.java
@@ -117,6 +117,8 @@ public class MethodComparisonMotifAnalysis {
 		if (Args.parseString(args,"region",null)!=null){
 			try{
 				restrictRegions = Args.parseRegions(args);
+				if (restrictRegions==null)
+					restrictRegions = new ArrayList<Region>();
 			}
 			catch (NotFoundException e){
 				// do nothing
