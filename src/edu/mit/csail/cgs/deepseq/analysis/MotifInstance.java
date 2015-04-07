0833b95c5c2dc404a473b80dd8fe1f6471c040d0
Yuchun Guo <yguo@mit.edu>
2015-04-06 21:45:44 -0400

    update

---
+++ src/edu/mit/csail/cgs/deepseq/analysis/MotifInstance.java
@@ -0,0 +1,11 @@
+package edu.mit.csail.cgs.deepseq.analysis;
+
+public class MotifInstance{
+	public int motifID;
+	public String motifName;
+	public int seqID;
+	public String matchSeq;
+	public int position;
+	public char strand;
+	public double score;
+}
